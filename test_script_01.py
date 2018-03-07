234a5c92754ab22b0d892cfea1848776ab5df36a
WojciechKusa <wojciech.kusa+github@gmail.com>
2018-03-07 21:16:30 +0100

    add more unit tests for task9

--- test_script_01.py
+++ test_script_01.py
@@ -12,6 +12,23 @@ class Test_task8(unittest.TestCase):
     def test_strings_a10(self):
         self.assertEqual( script_01.task8('a10'), 'one zero')
 
+    def test_strings_one51(self):
+        self.assertEqual( script_01.task8('one51'), 'one five one')
+
+
+class Test_task9(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_two_floats(self):
+        self.assertEqual( script_01.task9(3.5, 4.5), 4)
+
+    def test_list_params(self):
+        self.assertEqual( script_01.task9([1, 0, 2]), 1)
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
