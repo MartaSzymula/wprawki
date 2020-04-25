10277ed3024d48e1a53d11ed3b1637f0d83a5b1b
martaszy <martaszymula2@gmail.com>
2020-04-25 20:13:43 +0200

    corrected draw_card() method

--- games/blackjack.py
+++ games/blackjack.py
@@ -2,7 +2,7 @@ import random
 
 
 CARD_VALUES = {'2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, '10':10,
-'J':10, 'Q':10, 'K':10, 'A':11} # * 4 colors
+'J':10, 'Q':10, 'K':10, 'A':11}
 
 CARD_COLORS = ['♥️', '♦️', '♣️', '♠️']
 
@@ -12,9 +12,6 @@ class Card:
     def __init__(self, value, color):
         self.value = value
         self.color = color
-
-    # def draw_card(self, value, color):
-
 
 
     def show_card(self):
@@ -42,15 +39,13 @@ class Deck:
     def draw_card(self):
 
         # c = random.choice(list(self.cards))
-        c = self.cards[0]
+        c = list.pop(self.cards)
         c.show_card()
         return c
 
     def shuffle(self):
         random.shuffle(self.cards)
         return self.cards
-
-
 
 
 class Player:
@@ -78,12 +73,14 @@ if __name__ == '__main__':
 
 
     d = Deck()
-    d.shuffle()
-    d.draw_card()
+
+
 
 
      # Tu musi być jakaś pętla
-    # for i in range (0, 52):
+    for i in range (0, 52):
+        d.shuffle()
+        d.draw_card()
     #
     #     value = random.choice(list(CARD_VALUES))
     #     color = random.choice(list(CARD_COLORS))
